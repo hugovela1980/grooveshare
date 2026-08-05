15af592c41c3e1d412f490ebaf25925c881f27a9
Hugo Vela <54308271+hugovela1980@users.noreply.github.com>
2026-08-05 09:23:01 -0700

    Remove legacy frontend controllers

--- client/tests/run-tests.ts
+++ client/tests/run-tests.ts
@@ -1,6 +1,4 @@
 import "./test-runner/tester.test.js";
-import "./project-controller.test.js";
-import "./track-upload-controller.test.js";
 import "./app-router.test.js";
 import "./page-templates.test.js";
 import "./grooveshare-app.test.js";
